25122296c0c58e672982b0aef860ebbe2918391b
Sarah Schultz <sarahjschultz@gmail.com>
2024-07-30 10:40:46 -0600

    Another revision to approach for contains_duplicates after a month off

--- leetcode/contains_duplicates.rb
+++ leetcode/contains_duplicates.rb
@@ -1,31 +1,16 @@
 def contains_duplicates(nums)
-  count_map = {}
+  seen = {}
 
   nums.each do |n|
-    count_map[n] = count_map[n] ||= 0
-    count_map[n] += 1
-    return true if count_map[n] > 1
+    return true if seen.keys.include?(n)
+
+    seen[n] = true
   end
 
   false
 end
 
-def contains_duplicates(nums)
-  return false if [0, 1].include?(nums.length)
-
-  solution = false
-  counts = {}
-
-  nums.each do |n|
-    counts[n].nil? ? counts[n] = 1 : counts[n] += 1
-    solution = true if counts[n] > 1
-  end
-
-  solution
-
-end
-
-# nums = [1, 2, 3, 1]
-nums = [1,2,3,4]
+nums = [1, 2, 3, 1]
+# nums = [1, 2, 3, 4]
 
 contains_duplicates(nums)
