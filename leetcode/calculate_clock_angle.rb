0f5127aa02ba8432b787e5c23344f60448f77e0e
Sarah Schultz <sarahjschultz@gmail.com>
2024-07-31 11:44:10 -0600

    Small cleanup of angle problem that does not require splitting out all intermediate calcs in the hour angle computation

--- leetcode/calculate_clock_angle.rb
+++ leetcode/calculate_clock_angle.rb
@@ -1,28 +1,25 @@
 def angle_clock(hour, minutes)
-  min_angle  = 6
-  hour_angle = 30
+  deg_per_minute = 360 / 60
+  deg_per_hour = 360 / 12
 
-  angle_from_minutes = minutes * min_angle
-  calc_hour = hour % 12
-  contr_mins = (minutes.to_f / 60.0)
+  minutes_angle = minutes * deg_per_minute
+  hour_angle = ((hour % 12) + (minutes / 60.0)) * deg_per_hour
 
-  angle_from_hour = (calc_hour + contr_mins) * hour_angle
-
-  diff = (angle_from_hour - angle_from_minutes).abs
+  diff = (hour_angle - minutes_angle).abs
 
   [diff, 360 - diff].min
 end
 
-hour    = 12
-minutes = 30
+# hour    = 12
+# minutes = 30
 # output: 165
 
 # hour = 3
 # minutes = 30
 # # output: 75
 #
-# hour = 3
-# minutes = 15
+hour = 3
+minutes = 15
 # # output: 7.5
 
 angle_clock(hour, minutes)
